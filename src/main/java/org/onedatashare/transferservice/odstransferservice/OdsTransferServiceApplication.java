b4e827cedf16a862f912a5460746263670d4d7a6
Jacob <jgoldverg@gmail.com>
2020-07-02 14:55:19 -0400

    Initial Commit

---
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/OdsTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package org.onedatashare.transferservice.odstransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsTransferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/onedatashare/transferservice/odstransferservice/OdsTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onedatashare.transferservice.odstransferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdsTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
