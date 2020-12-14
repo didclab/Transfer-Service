efbceac26abb62a0248f13de585f526605d7c3a3
18vishal-singh <18vishal.singh@gmail.com>
2020-12-14 04:25:05 -0500

    Refined controller my making new service for setting JobParams

--- src/main/java/org/onedatashare/transferservice/odstransferservice/controller/TransferController.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/controller/TransferController.java
@@ -1,11 +1,9 @@
 package org.onedatashare.transferservice.odstransferservice.controller;
 
 import org.onedatashare.transferservice.odstransferservice.model.TransferJobRequest;
-import org.onedatashare.transferservice.odstransferservice.model.credential.AccountEndpointCredential;
-import org.onedatashare.transferservice.odstransferservice.model.credential.CredentialGroup;
-import org.onedatashare.transferservice.odstransferservice.model.credential.OAuthEndpointCredential;
 import org.onedatashare.transferservice.odstransferservice.service.DatabaseService.CrudService;
 import org.onedatashare.transferservice.odstransferservice.service.JobControl;
+import org.onedatashare.transferservice.odstransferservice.service.JobParamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobParameters;
@@ -21,14 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.onedatashare.transferservice.odstransferservice.constant.ODSConstants.*;
-
 /**
  * Transfer controller with to initiate transfer request
  */
 @RestController
 @RequestMapping("/api/v1/transfer")
 public class TransferController {
+
+    Logger logger = LoggerFactory.getLogger(TransferController.class);
 
     @Autowired
     JobLauncher jobLauncher;
@@ -42,51 +40,23 @@ public class TransferController {
     @Autowired
     JobLauncher asyncJobLauncher;
 
-    Logger logger = LoggerFactory.getLogger(TransferController.class);
-
-    @RequestMapping(value = "/start", method = RequestMethod.POST)
-    @Async
-    public ResponseEntity<String> start(@RequestBody TransferJobRequest request) throws Exception {
-        JobParameters parameters = translate(new JobParametersBuilder(), request);
-        crudService.insertBeforeTransfer(request);
-        logger.info(request.getSource().getParentInfo().getPath());
-        jc.setRequest(request);
-        jc.setChunkSize(request.getChunkSize());
-        logger.info(String.valueOf(request.getChunkSize()));
-        asyncJobLauncher.run(jc.concurrentJobDefinition(), parameters);
-        return ResponseEntity.status(HttpStatus.OK).body("Your batch job has been submitted with \n ID: " + request.getJobId());
-    }
+    @Autowired
+    JobParamService jobParamService;
 
     @Autowired
     CrudService crudService;
 
-    public JobParameters translate(JobParametersBuilder builder, TransferJobRequest request) {
-        logger.info("Request received : "+request.toString());
-        builder.addLong(TIME, System.currentTimeMillis());
-        builder.addString(OWNER_ID, request.getOwnerId());
-        builder.addString(PRIORITY, String.valueOf(request.getPriority()));
-        builder.addString(CHUNK_SIZE, String.valueOf(request.getChunkSize()));
-        if (CredentialGroup.ACCOUNT_CRED_TYPE.contains(request.getSource().getType())) {
-            AccountEndpointCredential credential = request.getSource().getVfsSourceCredential();
-            builder.addString(SOURCE_CREDENTIAL_ID, credential.getUsername());
-            builder.addString(SOURCE_URI, credential.getUri());
-            builder.addString(SOURCE_BASE_PATH, request.getSource().getParentInfo().getPath());
-        } else if (CredentialGroup.OAUTH_CRED_TYPE.contains(request.getSource().getType())) {
-            OAuthEndpointCredential oauthCred = request.getSource().getOauthSourceCredential();
-        }else{
-            return null;
-        }
-        if (CredentialGroup.ACCOUNT_CRED_TYPE.contains(request.getDestination().getType())) {
-            AccountEndpointCredential credential = request.getDestination().getVfsDestCredential();
-            builder.addString(DEST_CREDENTIAL_ID, credential.getUsername());
-            builder.addString(DEST_URI, credential.getUri());
-            builder.addString(DEST_BASE_PATH, request.getDestination().getParentInfo().getPath());
-        } else if (CredentialGroup.OAUTH_CRED_TYPE.contains(request.getDestination().getType())) {
-            OAuthEndpointCredential oauthCred = request.getDestination().getOauthDestCredential();
-        }else{
-            return null;
-        }
-        return builder.toJobParameters();
+    @RequestMapping(value = "/start", method = RequestMethod.POST)
+    @Async
+    public ResponseEntity<String> start(@RequestBody TransferJobRequest request) throws Exception {
+        logger.info("Controller Entry point");
+        JobParameters parameters = jobParamService.translate(new JobParametersBuilder(), request);
+        crudService.insertBeforeTransfer(request);
+        logger.info(request.getSource().getParentInfo().getPath());
+        jc.setRequest(request);
+        jc.setChunkSize(request.getChunkSize());
+        asyncJobLauncher.run(jc.concurrentJobDefinition(), parameters);
+        return ResponseEntity.status(HttpStatus.OK).body("Your batch job has been submitted with \n ID: " + request.getJobId());
     }
 }
 
---
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/JobParamService.java
@@ -0,0 +1,50 @@
+package org.onedatashare.transferservice.odstransferservice.service;
+
+import org.onedatashare.transferservice.odstransferservice.model.TransferJobRequest;
+import org.onedatashare.transferservice.odstransferservice.model.credential.AccountEndpointCredential;
+import org.onedatashare.transferservice.odstransferservice.model.credential.CredentialGroup;
+import org.onedatashare.transferservice.odstransferservice.model.credential.OAuthEndpointCredential;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.stereotype.Service;
+
+import static org.onedatashare.transferservice.odstransferservice.constant.ODSConstants.*;
+import static org.onedatashare.transferservice.odstransferservice.constant.ODSConstants.DEST_BASE_PATH;
+
+@Service
+public class JobParamService {
+
+    Logger logger = LoggerFactory.getLogger(JobParamService.class);
+
+    public JobParameters translate(JobParametersBuilder builder, TransferJobRequest request) {
+        logger.info("Setting job Parameters");
+        logger.info("Request received : " + request.toString());
+        builder.addLong(TIME, System.currentTimeMillis());
+        builder.addString(OWNER_ID, request.getOwnerId());
+        builder.addString(PRIORITY, String.valueOf(request.getPriority()));
+        builder.addString(CHUNK_SIZE, String.valueOf(request.getChunkSize()));
+        if (CredentialGroup.ACCOUNT_CRED_TYPE.contains(request.getSource().getType())) {
+            AccountEndpointCredential credential = request.getSource().getVfsSourceCredential();
+            builder.addString(SOURCE_CREDENTIAL_ID, credential.getUsername());
+            builder.addString(SOURCE_URI, credential.getUri());
+            builder.addString(SOURCE_BASE_PATH, request.getSource().getParentInfo().getPath());
+        } else if (CredentialGroup.OAUTH_CRED_TYPE.contains(request.getSource().getType())) {
+            OAuthEndpointCredential oauthCred = request.getSource().getOauthSourceCredential();
+        } else {
+            return null;
+        }
+        if (CredentialGroup.ACCOUNT_CRED_TYPE.contains(request.getDestination().getType())) {
+            AccountEndpointCredential credential = request.getDestination().getVfsDestCredential();
+            builder.addString(DEST_CREDENTIAL_ID, credential.getUsername());
+            builder.addString(DEST_URI, credential.getUri());
+            builder.addString(DEST_BASE_PATH, request.getDestination().getParentInfo().getPath());
+        } else if (CredentialGroup.OAUTH_CRED_TYPE.contains(request.getDestination().getType())) {
+            OAuthEndpointCredential oauthCred = request.getDestination().getOauthDestCredential();
+        } else {
+            return null;
+        }
+        return builder.toJobParameters();
+    }
+}
