710be76ecc63d936549d1365450d4ab1a40cfbec
Jacob Goldverg <jgoldverg@gmail.com>
2022-02-09 11:49:00 -0500

    chaning consumer to define queue based on APP name and User name

--- src/main/java/org/onedatashare/transferservice/odstransferservice/consumer/RabbitMQConsumer.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/consumer/RabbitMQConsumer.java
@@ -10,6 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -36,7 +39,10 @@ public class RabbitMQConsumer {
     CrudService crudService;
 
 
-    @RabbitListener(queues = "${ods.rabbitmq.queue}")
+    @RabbitListener(
+            bindings = @QueueBinding(exchange = @Exchange("${ods.rabbitmq.exchange}"),
+            value = @Queue("${ods.rabbitmq.queue}"),
+            key = "${ods.rabbitmq.queue}"))
     public void consumeDefaultMessage(final Message message) throws Exception {
         String jsonStr = new String(message.getBody());
         System.out.println("Message received");// + jsonStr);
