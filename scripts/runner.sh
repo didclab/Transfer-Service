e1a6678008340a83e530b79c3836db9c4363c8f9
Mohammad Faraz <farazm708@gmail.com>
2022-10-06 20:26:46 -0400

    Pulled from master and updated docker file to run on ECS

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -2,13 +2,14 @@
 
 # Requires some aws stuff
 export AWS_REGION=us-east-2
-aws s3 cp s3://<boot-file-path> /app/config/
-aws s3 cp s3://<certs-path> /app/certs/ --recursive
-chmod 400 /app/certs/* 
-
+aws s3 cp s3://<path-to-config>/config/boot.sh /app/config/
+aws s3 cp s3://<path-to-certs>/certs /app/certs/ --recursive
+chmod 600 /app/certs/* 
+chmod u+x /app/config/boot.sh
+ls -alR /app
 
 ## Source boot.sh from mounted directory
-
+sed -i "s/<node>/t3_ec2_medium/g" /app/config/boot.sh
 source /app/config/boot.sh
 
 # Run transfer-service
