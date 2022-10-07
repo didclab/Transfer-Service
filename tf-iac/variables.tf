46ae9264e963c23547364ae9f69ede2326cb6eab
Mohammad Faraz <farazm708@gmail.com>
2022-10-06 20:30:37 -0400

    resolved merge-conflict

--- tf-iac/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-    description = "Region to deploy resources in"
-    default = "us-east-2"
-}
-variable "image_id" {
-    description = "Image ID to use with launch template for ASG"
-}
