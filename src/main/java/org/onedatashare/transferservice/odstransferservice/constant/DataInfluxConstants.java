64013fd8df57f660625367a393b56571cdcada93
Jacob Goldverg <jgoldverg@gmail.com>
2023-02-06 14:26:09 -0500

    forgot to remove these long ago

--- src/main/java/org/onedatashare/transferservice/odstransferservice/constant/DataInfluxConstants.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/constant/DataInfluxConstants.java
@@ -8,18 +8,10 @@ public class DataInfluxConstants {
     public static final String CPU_FREQUENCY_MAX = "cpu_frequency_max";
     public static final String CPU_FREQUENCY_CURRENT = "cpu_frequency_current";
     public static final String CPU_FREQUENCY_MIN = "cpu_frequency_min";
-    public static final String ENERGY_CONSUMED = "energy_consumed";
     public static final String CPU_ARCHITECTURE = "cpu_arch";
     public static final String PACKET_LOSS_RATE = "packet_loss_rate";
-    public static final String LINK_CAPACITY = "link_capacity";
     public static final String BYTES_DOWNLOADED = "bytesDownloaded";
     public static final String BYTES_UPLOADED = "bytesUploaded";
-
-    // TODO - remove delta
-    public static final String BYTES_SENT_DELTA = "bytes_sent_delta";
-    public static final String BYTES_RECEIVED_DELTA = "bytes_received_delta";
-    public static final String PACKETS_SENT_DELTA = "packets_sent_delta";
-    public static final String PACKETS_RECEIVED_DELTA = "packets_received_delta";
 
     public static final String BYTES_SENT = "bytes_sent";
     public static final String BYTES_RECEIVED = "bytes_recv";
@@ -27,10 +19,7 @@ public class DataInfluxConstants {
     public static final String PACKETS_RECEIVED = "packets_recv";
     public static final String DROP_IN = "dropin";
     public static final String DROP_OUT = "dropout";
-    public static final String NIC_SPEED = "nic_speed";
     public static final String NIC_MTU = "nic_mtu";
-    public static final String P_METER_START_TIME = "pmeter_start_time";
-    public static final String P_METER_END_TIME = "pmeter_end_time";
     public static final String LATENCY = "latency";
     public static final String RTT = "rtt";
     public static final String SOURCE_RTT = "source_rtt";
@@ -41,7 +30,6 @@ public class DataInfluxConstants {
     public static final String ERROR_IN = "errin";
     public static final String ERROR_OUT = "errout";
     public static final String JOB_ID = "jobId";
-    public static final String THROUGHPUT = "throughput";
     public static final String CONCURRENCY = "concurrency";
     public static final String PARALLELISM = "parallelism";
     public static final String PIPELINING = "pipelining";
