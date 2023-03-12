5ecff8fe6874eddd8ef2673b45c87ac87a72bb3e
SravyaPo <99841571+SravyaPo@users.noreply.github.com>
2023-03-12 15:42:11 -0400

    changes

--- src/main/java/org/onedatashare/transferservice/odstransferservice/model/DeadLetterQueueData.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/model/DeadLetterQueueData.java
@@ -1,9 +1,43 @@
 package org.onedatashare.transferservice.odstransferservice.model;
 
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepExecution;
+
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
-public class DeadLetterQueueData {
+public class DeadLetterQueueData implements Serializable {
    public List <Throwable> failureException;
 
+   public JobParameters jobParameters;
 
+   public String failureString;
+
+   public Collection<StepExecution> stepExecutions;
+
+   public JobParameters getJobParameters() {
+      return jobParameters;
+   }
+
+   public void setJobParameters(JobParameters jobParameters) {
+      this.jobParameters = jobParameters;
+   }
+
+   public List<Throwable> getFailureException() {
+      return failureException;
+   }
+
+   public void setFailureException(List<Throwable> failureException) {
+      this.failureException = failureException;
+   }
+
+   public Collection<StepExecution> getStepExecutions() {
+      return stepExecutions;
+   }
+
+   public void setStepExecutions(Collection<StepExecution> stepExecutions) {
+      this.stepExecutions = stepExecutions;
+   }
 }
+
