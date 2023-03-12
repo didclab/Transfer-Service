c0145c9fea08fe76cf17ef39251130d33ea46e4b
SravyaPo <99841571+SravyaPo@users.noreply.github.com>
2023-03-12 15:42:41 -0400

    changes

--- src/main/java/org/onedatashare/transferservice/odstransferservice/service/DeadLetterQueueService.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/DeadLetterQueueService.java
@@ -4,24 +4,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.onedatashare.transferservice.odstransferservice.model.DeadLetterQueueData;
 import org.springframework.amqp.core.Message;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+@Service
 public class DeadLetterQueueService {
 
-    public DeadLetterQueueData getDataFromMesaage(Message message,  JsonProcessingException exception){
-        DeadLetterQueueData data = new DeadLetterQueueData();
-        data.failureException = new ArrayList<>();
-        data.failureException.add(exception);
-        return data;
-    }
-
-    public DeadLetterQueueData getDataFromJobExecution(JobParameters jobParameters, List<Throwable> failureExceptions){
+    public DeadLetterQueueData convertDataToDLQ(JobParameters jobParameters, List<Throwable> failureExceptions, Collection<StepExecution> stepExecutions){
         DeadLetterQueueData data = new DeadLetterQueueData();
         data.failureException = new ArrayList<>(failureExceptions);
+        data.jobParameters = jobParameters;
+        data.stepExecutions = new ArrayList<>(stepExecutions);
         return data;
     }
-
 
 }
