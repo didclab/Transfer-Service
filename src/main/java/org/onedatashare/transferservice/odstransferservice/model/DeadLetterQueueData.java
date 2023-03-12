df43a06a6bbd99438a246d029d833452dc61b0f2
SravyaPo <99841571+SravyaPo@users.noreply.github.com>
2023-03-12 17:21:26 -0400

    able to push to deadLetterQueue

--- src/main/java/org/onedatashare/transferservice/odstransferservice/model/DeadLetterQueueData.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/model/DeadLetterQueueData.java
@@ -1,5 +1,6 @@
 package org.onedatashare.transferservice.odstransferservice.model;
 
+import lombok.Data;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepExecution;
 
@@ -7,37 +8,10 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
+@Data
 public class DeadLetterQueueData implements Serializable {
    public List <Throwable> failureException;
-
    public JobParameters jobParameters;
-
-   public String failureString;
-
    public Collection<StepExecution> stepExecutions;
-
-   public JobParameters getJobParameters() {
-      return jobParameters;
-   }
-
-   public void setJobParameters(JobParameters jobParameters) {
-      this.jobParameters = jobParameters;
-   }
-
-   public List<Throwable> getFailureException() {
-      return failureException;
-   }
-
-   public void setFailureException(List<Throwable> failureException) {
-      this.failureException = failureException;
-   }
-
-   public Collection<StepExecution> getStepExecutions() {
-      return stepExecutions;
-   }
-
-   public void setStepExecutions(Collection<StepExecution> stepExecutions) {
-      this.stepExecutions = stepExecutions;
-   }
 }
 
