77f7e8ba179ac9c8b1150a3e85ed353b1c9c7fbf
SravyaPo <99841571+SravyaPo@users.noreply.github.com>
2023-05-07 12:41:20 -0400

    reauthentication changes

--- src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/box/BoxReader.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/box/BoxReader.java
@@ -2,10 +2,13 @@ package org.onedatashare.transferservice.odstransferservice.service.step.box;
 
 import com.box.sdk.BoxAPIConnection;
 import com.box.sdk.BoxFile;
+import com.box.sdk.BoxUser;
+import org.onedatashare.transferservice.odstransferservice.constant.ODSConstants;
 import org.onedatashare.transferservice.odstransferservice.model.DataChunk;
 import org.onedatashare.transferservice.odstransferservice.model.EntityInfo;
 import org.onedatashare.transferservice.odstransferservice.model.FilePart;
 import org.onedatashare.transferservice.odstransferservice.model.credential.OAuthEndpointCredential;
+import org.onedatashare.transferservice.odstransferservice.service.AuthenticateCredentials;
 import org.onedatashare.transferservice.odstransferservice.service.FilePartitioner;
 import org.onedatashare.transferservice.odstransferservice.utility.ODSUtility;
 import org.slf4j.Logger;
@@ -17,6 +20,8 @@ import org.springframework.util.ClassUtils;
 
 import java.io.ByteArrayOutputStream;
 
+import static org.onedatashare.transferservice.odstransferservice.constant.ODSConstants.OWNER_ID;
+
 public class BoxReader extends AbstractItemCountingItemStreamItemReader<DataChunk> {
 
     private OAuthEndpointCredential credential;
@@ -27,14 +32,24 @@ public class BoxReader extends AbstractItemCountingItemStreamItemReader<DataChun
     int retry;
     Logger logger = LoggerFactory.getLogger(BoxReader.class);
 
-    public BoxReader(OAuthEndpointCredential credential, EntityInfo fileInfo) {
+    String usersEmail;
+    String ownerId;
+
+    private AuthenticateCredentials authenticateCredentials;
+
+    public BoxReader(OAuthEndpointCredential credential, EntityInfo fileInfo, AuthenticateCredentials authenticateCredentials) {
         this.credential = credential;
         this.setName(ClassUtils.getShortName(BoxReader.class));
         filePartitioner = new FilePartitioner(fileInfo.getChunkSize());
         this.fileInfo = fileInfo;
         retry = 1;
+        this.authenticateCredentials = authenticateCredentials;
     }
 
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution){
+        this.ownerId = stepExecution.getJobParameters().getString(OWNER_ID);
+    }
     /**
      * Read in those chunks
      *
@@ -42,6 +57,10 @@ public class BoxReader extends AbstractItemCountingItemStreamItemReader<DataChun
      */
     @Override
     protected DataChunk doRead() {
+        if(this.credential.isTokenExpires()){
+            this.credential = authenticateCredentials.checkExpiryAndGenerateNew( usersEmail, ODSConstants.BOX, ownerId);
+            doOpen();
+        }
         FilePart filePart = filePartitioner.nextPart();
         if (filePart == null) return null;
         ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
@@ -60,6 +79,9 @@ public class BoxReader extends AbstractItemCountingItemStreamItemReader<DataChun
     protected void doOpen() {
         filePartitioner.createParts(this.fileInfo.getSize(), this.fileInfo.getId());
         this.boxAPIConnection = new BoxAPIConnection(credential.getToken());
+        BoxUser user = new BoxUser(this.boxAPIConnection, this.credential.getAccountId());
+        BoxUser.Info userInfo = user.getInfo("login");
+        this.usersEmail = userInfo.getLogin();
         this.currentFile = new BoxFile(this.boxAPIConnection, this.fileInfo.getId());
         this.currentFile.getInfo("name");
         this.boxAPIConnection.setMaxRetryAttempts(this.retry);
