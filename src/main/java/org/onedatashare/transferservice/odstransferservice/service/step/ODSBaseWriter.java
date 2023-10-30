2efb14e3ecfa40d578927e90d42424292510951d
Jacob Goldverg <jgoldverg@gmail.com>
2023-10-30 16:04:49 -0400

    Debug logging included

--- src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/ODSBaseWriter.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/ODSBaseWriter.java
@@ -3,6 +3,8 @@ package org.onedatashare.transferservice.odstransferservice.service.step;
 import org.onedatashare.transferservice.odstransferservice.model.DataChunk;
 import org.onedatashare.transferservice.odstransferservice.service.InfluxCache;
 import org.onedatashare.transferservice.odstransferservice.service.cron.MetricsCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.AfterRead;
 import org.springframework.batch.core.annotation.AfterWrite;
@@ -23,6 +25,7 @@ public class ODSBaseWriter {
     MetricsCollector metricsCollector;
 
     InfluxCache influxCache;
+    Logger logger = LoggerFactory.getLogger(ODSBaseWriter.class);
 
 
     public ODSBaseWriter(MetricsCollector metricsCollector, InfluxCache influxCache) {
@@ -35,6 +38,7 @@ public class ODSBaseWriter {
     @BeforeWrite
     public void beforeWrite() {
         LocalDateTime startWriteTime = LocalDateTime.now();
+        logger.info("Thread {} beforeWrite", Thread.currentThread());
         this.writeStartTimes.put(Thread.currentThread().getId(), startWriteTime);
     }
 
@@ -44,15 +48,17 @@ public class ODSBaseWriter {
         LocalDateTime writeEndTime = LocalDateTime.now();
         long totalBytes = items.stream().mapToLong(DataChunk::getSize).sum();
         LocalDateTime writeStartTime = this.writeStartTimes.get(Thread.currentThread().getId());
+        logger.info("Thread {} afterWrite", Thread.currentThread());
         //this is a cache for the optimizer directly in. This i actually think should be deleted and all data querying maybe ideally is done through the monitoring interface
         influxCache.addMetric(Thread.currentThread().getId(), stepExecution, totalBytes, writeStartTime, writeEndTime, InfluxCache.ThroughputType.WRITER, items.get(0).getSize());
-        System.gc();
+
     }
 
     @BeforeRead
     public void beforeRead() {
         LocalDateTime startReadTime = LocalDateTime.now();
         this.readStartTimes.put(Thread.currentThread().getId(), startReadTime);
+        logger.info("Thread {} beforeRead",Thread.currentThread());
     }
 
     @AfterRead
@@ -63,6 +69,7 @@ public class ODSBaseWriter {
         }
         LocalDateTime readStartTime = this.readStartTimes.get(Thread.currentThread().getId());
         if (readStartTime == null) return;
+        logger.info("Thread {} after Read", Thread.currentThread());
         influxCache.addMetric(Thread.currentThread().getId(), stepExecution, item.getSize(), readStartTime, endTime, InfluxCache.ThroughputType.READER, item.getSize());
     }
 }
