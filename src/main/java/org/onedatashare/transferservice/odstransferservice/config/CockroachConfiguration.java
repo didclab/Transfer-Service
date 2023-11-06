5e58ad6a2697b5fe2383d225f7ea75c64b00ea41
Jacob Goldverg <jgoldverg@gmail.com>
2023-11-06 13:34:04 -0500

    Ok using a cockroach config bc otherwise things r just weird

--- src/main/java/org/onedatashare/transferservice/odstransferservice/config/CockroachConfiguration.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/config/CockroachConfiguration.java
@@ -1,2 +1,28 @@
-package org.onedatashare.transferservice.odstransferservice.config;public class CockroachConfiguration {
+package org.onedatashare.transferservice.odstransferservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Profile("cockroach")
+@Configuration
+public class CockroachConfiguration {
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.addScript(new ClassPathResource("org/springframework/batch/core/schema-postgresql.sql"));
+        databasePopulator.setContinueOnError(true);
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator);
+        initializer.setEnabled(true);
+        return initializer;
+    }
+
 }
